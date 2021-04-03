b189c50cc35f9be961a6f790f739eecd996b7742
mbish08 <mdrew8@gmail.com>
2021-04-03 12:23:40 -0700

    updated routes with namespacing

--- dogwalk_api_backend/config/routes.rb
+++ dogwalk_api_backend/config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :dogs, only: [:index]
-  resources :owners, only: [:index]
+  namespace :api do
+    namespace :v1 do
+      resources :dogs, only: [:index]
+      resources :owners, only: [:index]
+    end
+  end
+  
 end
