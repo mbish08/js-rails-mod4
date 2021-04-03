35b80e73ffaf9630e2e76a392cb4c661aea39d13
mbish08 <mdrew8@gmail.com>
2021-04-03 12:59:55 -0700

    update create action

--- dogwalk_api_backend/app/controllers/api/v1/dogs_controller.rb
+++ dogwalk_api_backend/app/controllers/api/v1/dogs_controller.rb
@@ -7,6 +7,11 @@ class Api::V1::DogsController < ApplicationController
 
     def create
         dog = Dog.new(dog_params)
+        if dog.save
+            render json: dog, status: :accepted
+        else
+            render json: { errors: dog.errors.full_messages }, status: :unprocessible_entity
+        end
     end
 
     private 
@@ -14,5 +19,5 @@ class Api::V1::DogsController < ApplicationController
     def dog_params
         params.require(:dog).permit(:name, :breed, :weight, :allergies, :behaviors, :meds, :vet_info, :age, :owner_id)
     end
-    
+
 end
