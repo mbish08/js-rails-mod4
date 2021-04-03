a39ad886393cf9c532e150a2bb1b3e75aeff9b10
mbish08 <mdrew8@gmail.com>
2021-04-03 12:55:29 -0700

    add create action and strong params

--- dogwalk_api_backend/app/controllers/api/v1/dogs_controller.rb
+++ dogwalk_api_backend/app/controllers/api/v1/dogs_controller.rb
@@ -4,4 +4,15 @@ class Api::V1::DogsController < ApplicationController
         dogs = Dog.all
         render json :dogs
     end
+
+    def create
+        dog = Dog.new(dog_params)
+    end
+
+    private 
+
+    def dog_params
+        params.require(:dog).permit(:name, :breed, :weight, :allergies, :behaviors, :meds, :vet_info, :age, :owner_id)
+    end
+    
 end
