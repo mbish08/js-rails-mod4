ce4944bb83c79906865f6d244f1ad2b6afde918a
mbish08 <mdrew8@gmail.com>
2021-04-02 17:13:01 -0700

    owner model, migration, seeds

---
+++ dogwalk_api_backend/app/models/owner.rb
@@ -0,0 +1,4 @@
+class Owner < ApplicationRecord
+    #! has_many :Dogs
+
+end
--- dogwalk_api_backend/db/seeds.rb
+++ dogwalk_api_backend/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#* Owner seeds
+harry = Owner.create(name: "Harry")
+ron = Owner.create(name: "Ron")
+hermione = Owner.create(name: "Hermione")
