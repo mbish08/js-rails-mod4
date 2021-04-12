c8dca2a63558bb0d1f7866a53f3692091e75203b
mbish08 <mdrew8@gmail.com>
2021-04-12 14:25:42 -0700

    new owner form initial set up

--- dogwalk_frontend/index.js
+++ dogwalk_frontend/index.js
@@ -1,8 +1,16 @@
 const ownersAdapter = new OwnersAdapter
 const dogsAdapter = new DogsAdapter
+const newOwnerButton = document.getElementById('new-owner-button')
 
 document.addEventListener('DOMContentLoaded', () => {
     ownersAdapter.fetchOwners()
     dogsAdapter.fetchDogs()
+    newOwnerButton.addEventListener('click', newOwnerForm)
 })
+
+function newOwnerForm(e) {
+    e.target.hidden = true
+    const newOwnerForm = document.getElementById('new-owner-form-container')
+    newOwnerForm.hidden = false
+}
 
