29ac624d01fd89fa4cee3a832bdb6e7701016dfd
mbish08 <mdrew8@gmail.com>
2021-04-13 13:16:20 -0700

    add destroy to routes

--- dogwalk_api_backend/config/routes.rb
+++ dogwalk_api_backend/config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :dogs, only: [:index, :create]
-      resources :owners, only: [:index, :create]
+      resources :owners, only: [:index, :create, :destroy]
     end
   end
   
