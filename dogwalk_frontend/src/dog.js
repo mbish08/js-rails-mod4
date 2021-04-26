a29a2b18203b2934b778bdf4f43a879c0f235d06
mbish08 <mdrew8@gmail.com>
2021-04-26 12:18:31 -0700

    new dogs update on dom without refresh

--- dogwalk_frontend/src/dog.js
+++ dogwalk_frontend/src/dog.js
@@ -19,9 +19,31 @@ class Dog {
         Dog.all.push(this)
     }
 
-    dogToDom() {
-        console.log(this)
+    dogToDom = () => {
+        // console.log(this)
         // debugger
+        const dogsList = document.getElementById(`owner-${this.owner_id}`)
+        dogsList.element = document.createElement('div')
+        dogsList.element.className = "dogs"
+
+
+            dogsList.innerHTML += `
+            <div id="dog-${this.id}">
+                <h4>${this.name}</h4>
+                <button class="delete-dog" data-id="${this.id}">Delete ${this.name}</button>
+                <ul>
+                    <li>Breed: ${this.breed ? this.breed : 'none'}</li>
+                    <li>Weight: ${this.weight ? this.weight : 'none'}</li>
+                    <li>Allergies: ${this.allergies ? this.allergies : 'none'}</li>
+                    <li>Behaviors: ${this.behaviors ? this.behaviors : 'none'}</li>
+                    <li>Meds: ${this.meds ? this.meds : 'none'}</li>
+                    <li>Vet Info: ${this.vet_info ? this.vet_info : 'none'}</li>
+                    <li>Age: ${this.age ? this.age : 'none'}</li>
+                </ul>
+            </div>
+            `
+            
+            // this.ownerList.appendChild(dogsList)
         
     }
 
